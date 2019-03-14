6c0d840be8a47e1622c963389f77e02fd2d8effc
Yangtao.jiang <jiangyangtao1@outlook.com>
2019-03-15 01:33:37 +0800

    Add randomRange func

--- src/commons/passwordGenerator.js
+++ src/commons/passwordGenerator.js
@@ -3,9 +3,13 @@ const key = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const symbol = '~!@#$%^&*()_+`-=';
 const atSymbol = '@';
 const joinAtSymbol = function joinAtSymbol(password) {
-  const key = password.substr(Math.randomRange(0, password.length) - 1, 1);
+  const key = password.substr(randomRange(0, password.length) - 1, 1);
   return password.replace(key, atSymbol);
 }
+
+const randomRange = function randomRange(min, max) {
+  return Math.floor(Math.random() * ((max - min) + 1)) + min;
+};
 
 passwordGenerator.newPassword = function newPassword(length = 8, containsAtSymbol = false, containsSymbol = false) {
   const _key = key;
@@ -13,7 +17,7 @@ passwordGenerator.newPassword = function newPassword(length = 8, containsAtSymbo
 
   let passwordStr = '';
   for (let index = 0; index < length; index++) {
-    passwordStr += _key.substr(Math.randomRange(0, _key.length) - 1, 1);
+    passwordStr += _key.substr(randomRange(0, _key.length) - 1, 1);
   }
   if(containsAtSymbol && passwordStr.indexOf('@') === -1) {
     passwordStr = joinAtSymbol(passwordStr);
